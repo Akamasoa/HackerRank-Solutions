69e587156676a1d19671fdbc7c92de22301209ca
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-10-12 21:31:26 -0300

    Update Day 0: Hello, World.js

--- 30 Days of Code/Day 0: Hello, World.js
+++ 30 Days of Code/Day 0: Hello, World.js
@@ -32,3 +32,10 @@ Explanation
 On the first line, we print the string literal Hello, World.. On the second line, we print the contents of the  variable which, for this sample 
 case, happens to be Welcome to 30 Days of Code!. If you do not print the variable's contents to stdout, you will not pass the hidden test case.
 */
+
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    // Write the second line of output that prints the contents of 'inputString' here.
+    console.log(inputString);
+}
